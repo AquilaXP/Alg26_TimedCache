5d62997dfef9b51c58f8afc8b9c666756a580b02
Unknown <aquila-2004@yandex.ru>
2019-12-22 23:33:14 +0300

    Доработан кэш со стороны многопоточной работы
    - Теперь удаление объектов выполняется так же в отдельном потоке;
    - Добавлены дополнительные unit-тесты.

--- Test/Test.cpp
+++ Test/Test.cpp
@@ -1,6 +1,7 @@
 #include <thread>
 #include <string>
 #include <iostream>
+#include <random>
 
 #include "TimedCache.h"
 
@@ -9,6 +10,7 @@
 
 void test_set_get()
 {
+    std::cout << __func__ << std::endl;
     // Таймер взвинчиваем, чтобы проверить работу set и get
     TimedCache< int, std::string > cache( 10, std::chrono::seconds( 10000 ) );
 
@@ -58,6 +60,7 @@ void test_set_get()
 
 void test_timer()
 {
+    std::cout << __func__ << std::endl;
     TimedCache< int, std::string > cache( 100, std::chrono::seconds( 1 ) );
 
     std::vector< std::pair< int, std::string > > test_data( size_t( 10 ) );
@@ -100,12 +103,139 @@ void test_timer()
     }
 }
 
+void test_update()
+{
+    std::cout << __func__ << std::endl;
+    TimedCache< int, std::string > cache( 100, std::chrono::seconds( 1 ) );
+
+    std::vector< std::pair< int, std::string > > test_data( size_t( 10 ) );
+    int i = 0;
+    std::generate( test_data.begin(), test_data.end(),
+        [&]()-> std::pair< int, std::string >
+    {
+        ++i;
+        return std::make_pair( i, std::to_string( i ) );
+    } );
+
+    for( auto& v : test_data )
+        cache.set( v.first, v.second );
+
+    // зарпашиваем один и тот же объект, его время должно обновлятся,
+    // поэтому он всегда будет.
+    for( size_t i = 0; i < 10; ++i )
+    {
+        std::this_thread::sleep_for( std::chrono::milliseconds( 500 ) );
+        if( !cache.get( 1 ).has_value() )
+            T_ERROR( "dont update timer!" );
+    }
+    // Ждем чтобы все объекты "протухли"
+    std::this_thread::sleep_for( std::chrono::seconds( 2 ) );
+
+    // Теперь недолжно быть ни одного объекта
+    for( size_t i = 0; i < test_data.size(); ++i )
+    {
+        auto& v = test_data[i];
+        if( auto fv = cache.get( v.first ); fv.has_value() )
+        {
+            T_ERROR( "didnt delete element from cache!" );
+        }
+    }
+}
+
+void test_thread_cleaner()
+{
+    std::cout << __func__ << std::endl;
+    TimedCache< int, std::string > cache( 100, std::chrono::seconds( 1 ) );
+
+    std::vector< std::pair< int, std::string > > test_data( size_t( 10 ) );
+    int i = 0;
+    std::generate( test_data.begin(), test_data.end(),
+        [&]()-> std::pair< int, std::string >
+    {
+        ++i;
+        return std::make_pair( i, std::to_string( i ) );
+    } );
+
+    for( auto& v : test_data )
+        cache.set( v.first, v.second );
+
+    // Ждем чтобы все объекты "протухли"
+    std::this_thread::sleep_for( std::chrono::seconds( 2 ) );
+
+    if( cache.size() != 0 )
+        T_ERROR( "cleaner doesn't work!" );
+}
+
+void test_multithreading()
+{
+    std::cout << __func__ << std::endl;
+    TimedCache< int, std::string > cache( 1000, std::chrono::seconds( 1 ) );
+
+    std::vector< std::pair< int, std::string > > test_data( size_t( 10 ) );
+    int i = 0;
+    std::generate( test_data.begin(), test_data.end(),
+        [&]()-> std::pair< int, std::string >
+    {
+        ++i;
+        return std::make_pair( i, std::to_string( i ) );
+    } );
+
+    std::vector< std::thread > threads;
+    struct Worker
+    {
+        Worker( TimedCache<int, std::string>& cache, size_t countIteration )
+            : m_cache( cache ), m_CountIteration( countIteration )
+        {}
+        Worker( Worker&& other ) 
+            : m_cache( other.m_cache ), m_CountIteration(other.m_CountIteration)
+        {}
+        ~Worker()
+        {
+            if( m_thread.joinable() )
+                m_thread.join();
+        }
+        void start()
+        {
+            m_thread = std::thread( &Worker::execute, this );
+        }
+        void execute()
+        {
+            std::mt19937 mt{ std::default_random_engine() };
+            std::uniform_int_distribution dist( 0, 1000 );
+            for( size_t i = 0; i < m_CountIteration; ++i )
+            {
+                int v = dist( mt );
+                if( !m_cache.get( v ).has_value() )
+                    m_cache.set( v, std::to_string( v ) );
+            }
+        }
+    private:
+        size_t m_CountIteration = 0;
+        TimedCache<int, std::string>& m_cache;
+        std::thread m_thread;
+    };
+    
+    std::vector< Worker > workers;
+    for( size_t i = 0; i < std::thread::hardware_concurrency(); ++i )
+    {
+        workers.emplace_back( cache, 10000 );
+    }
+    std::for_each( std::begin( workers ), std::end( workers ), std::mem_fn( &Worker::start ) );
+    workers.clear();
+    std::this_thread::sleep_for( std::chrono::seconds( 2 ) );
+    if( cache.size() != 0 )
+        T_ERROR( "incorect work cache!" );
+}
+
 int main()
 {
     try
     {
         test_set_get();
         test_timer();
+        test_update();
+        test_thread_cleaner();
+        test_multithreading();
     }
     catch( const std::exception& err )
     {
--- TimedCache/TimedCache.h
+++ TimedCache/TimedCache.h
@@ -2,6 +2,10 @@
 
 #include <cstdint>
 
+#include <atomic>
+#include <condition_variable>
+#include <thread>
+#include <mutex>
 #include <map>
 #include <optional>
 #include <algorithm>
@@ -9,60 +13,106 @@
 #include <utility>
 #include <vector>
 
+// Вспомогательный класс, который позволяет усыплять и будить поток
+class Sleeper
+{
+    std::mutex m_lock;
+    std::condition_variable m_cond;
+    std::atomic_bool flag = ATOMIC_VAR_INIT( false );
+public:
+    template< class Rep, class Period >
+    void sleep( const std::chrono::duration< Rep, Period >& relTime )
+    {
+        std::unique_lock ul( m_lock );
+        m_cond.wait_for( ul, relTime, [&](){ return flag.load(); } );
+        flag = false;
+    }
+
+    void wakeUp()
+    {
+        flag = true;
+        m_cond.notify_one();
+    }
+};
+
 template< class K, class T >
 class TimedCache
 {
-    using nsec = uint64_t;
+    using tick = std::chrono::nanoseconds;
     using timer = std::chrono::high_resolution_clock::time_point;
 public:
     template< class Rep, class Period >
     TimedCache( size_t size, const std::chrono::duration< Rep, Period >& relTime )
-        : m_maxDTime( std::chrono::duration_cast<std::chrono::nanoseconds>( relTime ).count() ),
+        : m_maxDTime( std::chrono::duration_cast<std::chrono::nanoseconds>( relTime ) ),
         m_size( size )
     {
+        startClenaer();
     }
     TimedCache( const TimedCache& ) = delete;
     TimedCache& operator = ( const TimedCache& ) = delete;
+    ~TimedCache()
+    {
+        stopCleaner();
+    }
 
     std::optional< T > get( const K& key )
     {
+        std::lock_guard lg( m_lock );
         // Выполняем поиск
         auto iter = m_data.find( key );
         if( iter == m_data.end() )
             return std::optional< T >();
 
-        nsec dt =
-            std::chrono::duration_cast<std::chrono::nanoseconds>(
-                getCurrTime() - iter->second.second
-                ).count();
+        auto dt =
+            std::chrono::duration_cast<tick>(
+                getCurrTime() - iter->second.second );
+
+        T data = std::move( iter->second.first );
+        // Да, удаляем
+        m_key.erase( iter->second.second );
+        m_data.erase( iter );
+
         // Пройденно время превышает максимальное?
         if( dt >= m_maxDTime )
         {
-            // Да, удаляем, возращаем nil
-            m_key.erase( iter->second.second );
-            m_data.erase( iter );
+            if( m_data.size() == 0 )
+                stopCleaner();
             return std::optional< T >();
         }
 
+        auto currTime = getCurrTime();
+        auto [obj, state] = m_data.insert( std::make_pair( key, std::make_pair( std::move( data) , currTime ) ) );
+        m_key.insert( std::make_pair( currTime, key ) );
+
         // Все ок, возращаем объект
-        return std::optional< T >( iter->second.first );
+        return std::optional< T >( obj->second.first );
     }
     void set( const K& key, const T& value )
     {
-        // Чистим, если размер кэша превышен
-        if( m_data.size() == m_size )
+        bool wakeup = false;
         {
-            // В m_key у нас отсортированы по времени, берем первый.
-            auto old = m_key.begin();
-            m_data.erase( old->second );
-            m_key.erase( old );
+            std::lock_guard lg( m_lock );
+           // Чистим, если размер кэша превышен
+            if( m_data.size() == m_size )
+            {
+                // В m_key у нас отсортированы по времени, берем первый.
+                auto old = m_key.begin();
+                m_data.erase( old->second );
+                m_key.erase( old );
+            }
+            auto curTime = getCurrTime();
+            m_key[curTime] = key;
+            m_data[key] = std::make_pair( value, curTime );
+            if( m_data.size() == 1 )
+                wakeup = true;
         }
-        auto curTime = getCurrTime();
-        m_key[curTime] = key;
-        m_data[key] = std::make_pair( value, curTime );
+        // Появились новые объекты, надо разбудить поток
+        if( wakeup )
+            wakeUp();
     }
     size_t size() const
     {
+        std::lock_guard lg( m_lock );
         return m_data.size();
     }
     size_t capacity() const
@@ -71,17 +121,71 @@ public:
     }
     void clear()
     {
+        std::lock_guard lg( m_lock );
         m_key.clear();
         m_data.clear();
     }
 private:
+    void wakeUp()
+    {
+        m_sleeper.wakeUp();
+    }
     timer getCurrTime() const
     {
         return std::chrono::high_resolution_clock::now();
     }
+    void startClenaer()
+    {
+        m_working = true;
+        m_cleaner = std::thread( &TimedCache<K, T>::updateCached, this );
+    }
+    void stopCleaner()
+    {
+        m_working = false;
+        m_sleeper.wakeUp();
+        if( m_cleaner.joinable() )
+            m_cleaner.join();
+    }
+    void updateCached()
+    {
+        tick timeForSleep;
+        while( m_working )
+        {
+            {
+                timeForSleep = std::chrono::duration_cast< tick >( std::chrono::hours( 10 ) );
+                std::lock_guard lg( m_lock );
+                auto curTime = getCurrTime();
+                // если ключей нет, засыпаем на большой период
+                while( !m_key.empty() )
+                {
+                    auto first = m_key.begin();
+                    auto&[time, key] = *first;
+                    auto dt = std::chrono::duration_cast<tick>( curTime - time );
+                    if( dt >= m_maxDTime )
+                    {
+                        m_data.erase( key );
+                        m_key.erase( first );
+                    }
+                    else
+                    {
+                        timeForSleep = m_maxDTime - dt;
+                        // все следующие объекты "достаточно свежие"
+                        break;
+                    }
+                }
+            }
+            // спим, до следующего "протукшего" объекта или надолго, т.к. их нету
+            m_sleeper.sleep( timeForSleep );
+        }
+    }
 
+    Sleeper m_sleeper;
+    std::thread m_cleaner;
+    std::atomic_bool m_working = ATOMIC_VAR_INIT(false);
+
+    mutable std::mutex m_lock;
     size_t m_size = 0;
-    nsec m_maxDTime{};
+    tick m_maxDTime{};
     std::map< K, std::pair< T, timer > > m_data;
     std::map< timer, K > m_key;
 };
